39f5f1263f32ba729cb5ce1fb33a3b8c90a2708d
Omid Mirab <48311220+OmidMirab@users.noreply.github.com>
2019-05-01 11:54:28 +0430

    my first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] cars = new String[]{"BMW", "Volvo", "ford", "mazda"};
+        for (String C : cars) {
+            System.out.println(C);
+        }
+    }
+}
